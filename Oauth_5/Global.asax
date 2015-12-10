ff33d54649ea2bc10edb8dd45ddb20df9e152d7c
phuoctuong <phuoctuong285@gmail.com>
2015-12-10 10:47:23 +0700

    oauth_5

---
+++ Oauth_5/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Oauth_5.WebApiApplication" Language="C#" %>
---
+++ Oauth_5/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(Oauth_5.Startup))]
+
+namespace Oauth_5
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
